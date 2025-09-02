3b78f8b0dacaa1ef69403fd26634a5842e0ec054
Subramani Mott <gahanbasappamottana@gmail.com>
2025-09-02 03:05:33 +0000

    commit weather

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -1,11 +1,28 @@
 with daily_weather as (
 
-select * 
+select 
+
+date(time) as daily_weather,
+weather,
+temp,
+preasure,
+humidity,
+clouds
 
 from 
-{{ source('DEMO', 'WEATHER') }}
+{{ source('DEMO', 'WEATHER') }} 
+),
+daily_weather_agg as (
 
+    select 
+    daily_weather,
+    weather,
+    count(weather)
+    from daily_weather
+
+    group by daily_weather, weather
 
 )
 
-Select * from daily_weather
+
+Select * from daily_weather_agg
